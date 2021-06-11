32052cc421f57ce50912a9d9c7d310ea09e721fd
Garry Hall <garrymhall@gmail.com>
2021-06-11 16:11:51 +0100

    build database

---
+++ db/song_requestor.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS requests;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS songs;
+DROP TABLE IF EXISTS artists;
+
+
+
+CREATE TABLE artists (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE songs (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  artist_id INT REFERENCES artists(id),
+  last_played TIMESTAMP
+);  
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255),
+  bio TEXT
+);
+
+CREATE TABLE requests (
+    id SERIAL PRIMARY KEY,
+    song_id INT REFERENCES songs(id),
+    user_id INT REFERENCES artists(id),
+    time TIMESTAMP,
+    played BOOLEAN
+)
